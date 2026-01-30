2c77a1eaec85254958e4c4532c73fd9cbdf89578
Piero010405 <psarmientoayala@gmail.com>
2026-01-30 12:43:17 -0500

    adding mls

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -9,9 +9,11 @@ export async function GET(req: NextRequest) {
     const limitParam = searchParams.get("limit");
     const limit = limitParam ? parseInt(limitParam, 10) : 10;
 
+    const t0 = Date.now();
     const items = await searchProductsService(search, limit);
+    const t1 = Date.now();
 
-    return NextResponse.json({ items }, { status: 200 });
+    return NextResponse.json({ items, ms: { total: t1 - t0 } }, { status: 200 });
   } catch (error) {
     console.error("Error GET /api/products:", error);
     return NextResponse.json(
--- lib/repositories/products-repository.ts
+++ lib/repositories/products-repository.ts
@@ -2,28 +2,6 @@
 import { supabase } from "@/lib/db/supabase";
 import type { Product } from "@/lib/domain/product";
 
-export async function findProductsByDescriptionFTS(
-  search: string,
-  limit = 10
-): Promise<Product[]> {
-  const { data, error } = await supabase.rpc("search_products_fts", {
-    search_text: search,
-    lim: limit,
-  });
-
-  if (error) {
-    console.error("Supabase FTS error:", error);
-    throw new Error("Error consultando productos");
-  }
-
-  return (
-    data?.map((row: any) => ({
-      productId: row.product_id,
-      description: row.description,
-    })) ?? []
-  );
-}
-
 export async function findProductsByDescription(
   search: string,
   limit = 10
@@ -45,28 +23,3 @@ export async function findProductsByDescription(
     })) ?? []
   );
 }
-
-// export async function findProductsByDescription(
-//   search: string,
-//   limit = 10
-// ): Promise<Product[]> {
-
-//   const { data, error } = await supabase
-//     .from("form_alternativas_imdb")
-//     .select("product_id, description")
-//     .ilike("description", `${search}%`) // prefijo, más rápido
-//     .order("description", { ascending: true })
-//     .limit(limit);
-
-//   if (error) {
-//     console.error("Supabase error:", error);
-//     throw new Error("Error consultando Supabase");
-//   }
-
-//   return (
-//     data?.map((row) => ({
-//       productId: row.product_id,
-//       description: row.description,
-//     })) ?? []
-//   );
-// }
